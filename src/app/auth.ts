5c3539f78892681cadb615990eb574e7e9febcb4
nkkn-prog <nkkn.prog@gmail.com>
2024-12-30 14:02:50 +0900

    feat: authファイルの修正

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,19 +1,27 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import { NextAuthConfig } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/lib/prisma"
+import Sendgrid from "next-auth/providers/sendgrid"
+
 
 export const authConfig: NextAuthConfig = {
+  adapter: PrismaAdapter(prisma),
   providers: [
-    GitHub({
-      clientId: process.env.AUTH_GITHUB_ID!,
-      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+    Sendgrid({
+      apiKey: process.env.AUTH_SENDGRID_KEY!,
     }),
   ],
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnTodo = nextUrl.pathname.startsWith('/todo');
-      if (isOnTodo) {
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false;
       }
