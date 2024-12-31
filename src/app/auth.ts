85767743d30df28350daf24ea6b55d53bf78fb89
nkkn-prog <nkkn.prog@gmail.com>
2025-01-01 00:34:56 +0900

    feat: auth ユーザーに選択肢を表示させる処理の追加

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,33 +1,28 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { NextAuthConfig } from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "@/lib/prisma"
 
 export const authConfig: NextAuthConfig = {
-  adapter: PrismaAdapter(prisma),
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID!,
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+      authorization: {
+        // ユーザーに選択肢を表示する
+        params: {
+          prompt: "select_account",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     }),
   ],
   callbacks: {
-    async signIn({ user, account, profile }) {
-      // ユーザーデータの処理
-      if (account?.provider === "google") {
-        user.name = profile?.name
-        user.email = profile?.email
-        // 必要に応じて他のプロファイル情報も追加
-      }
+    async signIn({ user, account }) {
+      console.log(user, account)
       return true
-    },
-    async session({ session, user }) {
-      // セッションにユーザー情報を追加
-      session.user.id = user.id
-      return session
-    },
-  },
+    }
+  }
 };
 
 export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
