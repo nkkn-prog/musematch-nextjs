6b2edbc5c0977e97090a8b927ebfa682db9c3d23
nkkn-prog <nkkn.prog@gmail.com>
2025-01-01 10:57:57 +0900

    refactor: credentialのコメントアウト

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -8,13 +8,8 @@ export const signInWithGoogleOath = async () => {
 
 export const signUpWithGoogleOath = async () => {
   await signIn("google")
-} 
+}
 
-// export const signInWithEmail = async (values: {email: string, password: string}) => {
-//   const existingUser = await prisma.user.findUnique({
-//     where: { email: values.email! }
-//   });
-//   if (!existingUser) {
-//     return '/auth/error?error=NOT_REGISTERED';
-//   }
-// } 
+// export const signInWithCredentials = async () => {
+//   await signIn('credential')
+// }
