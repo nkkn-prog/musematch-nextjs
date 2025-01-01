53b070175f1a2fcbe6cf2ec218beaff777d89f44
nkkn-prog <nkkn.prog@gmail.com>
2025-01-01 10:58:17 +0900

    refactor: ソースの削除

--- src/app/api/auth/signup/google/route.ts
+++ src/app/api/auth/signup/google/route.ts
@@ -1,15 +1,1 @@
-import { auth } from "@/app/auth"
-import { redirect } from "next/navigation"
-
-export async function GET(request: Request) {
-  const session = await auth()
-  
-  if (!session?.user?.email) {
-    redirect('/auth/error?error=NO_USER')
-  }
-
-  // ここでユーザー登録処理を実行
-  // ...
-
-  redirect('/dashboard')
-} 
+ 
