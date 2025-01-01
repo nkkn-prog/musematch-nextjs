02d93f777538242fd699f3ae085aefdc4115b23b
nkkn-prog <nkkn.prog@gmail.com>
2025-01-01 10:59:55 +0900

    refactor: credentialの削除

--- src/app/components/sign-in.tsx
+++ src/app/components/sign-in.tsx
@@ -1,19 +1,17 @@
 import { signInWithGoogleOath } from "../actions/auth"
-import { PasswordInput, Button, TextInput, Box, Title, Text } from '@mantine/core';
-import { useForm } from '@mantine/form';
+import { Button, Box, Title, Text } from '@mantine/core';
 import { useHover } from '@mantine/hooks';
 import Image from "next/image";
-
-
+// import { signInWithCredentials } from "../actions/auth";
 
 export const SignInWithGoogle = ({errorMessage}:{errorMessage:string | null}) => {
-  // const { hovered, ref } = useHover();
+  const { hovered, ref } = useHover();
   return (
     <Box mt='xl'>
       <Title order={4} mb="xl" ta='center'>外部サービスでサインイン</Title>
       <form action={signInWithGoogleOath}>
-        <Box mt="md" ta='center'>
-          <Button type="submit">
+        <Box mt="md" ta='center' mb="xl" ref={ref}>
+          <Button type="submit" w='100%' bg={ hovered ? '#f6f6f6' : 'white'} c='#000' bd='1px solid #000'>
             <Image src="/logo/google-logo.png" alt="Google" width={20} height={20} />
             <Text ml='sm'>Googleでサインイン</Text>
           </Button>
@@ -24,48 +22,63 @@ export const SignInWithGoogle = ({errorMessage}:{errorMessage:string | null}) =>
   )
 }
 
-export const SignInWithEmail = () => {
-  const { hovered, ref } = useHover();
-  const form = useForm({
-    mode: 'uncontrolled',
-    initialValues: {
-      email: '',
-      password: '',
-      termsOfService: false,
-    },
+// export const SignInWithEmail = () => {
+//   const { hovered, ref } = useHover();
+//   const form = useForm({
+//     mode: 'uncontrolled',
+//     initialValues: {
+//       email: '',
+//       password: '',
+//       termsOfService: false,
+//     },
 
-    validate: {
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-    },
-  });
+//     validate: {
+//       email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+//     },
+//   });
 
-  return (
-    <Box mt='xl'>
-      <Title order={4} mb="md" ta='center'>メールアドレスでサインイン</Title>
-      <form onSubmit={form.onSubmit((values) => signInWithCredentials(values.email, values.password))}>
-        <TextInput
-          withAsterisk
-          label="Email"
-          placeholder="your@email.com"
-          key={form.key('email')}
-          {...form.getInputProps('email')}
-          mb="md"
-        />
-        <PasswordInput
-          withAsterisk
-          label="Password"
-          placeholder="password"
-          key={form.key('password')}
-          {...form.getInputProps('password')}
-        />
-        <Box mt="md" ta='center' mb="xl" ref={ref}>
-          {/* 未対応:ホバーした時にボタンの色を変える */}
-          <Button type="submit" color="navy" w='100%' bg={hovered ? 'navy' : 'navy'}>
-            ログイン
-          </Button>
-        </Box>
-      </form>
-    </Box>
-  )
-}
+//   return (
+//     <Box mt='xl'>
+//       <Title order={4} mb="md" ta='center'>メールアドレスでサインイン</Title>
+//       <form onSubmit={form.onSubmit((values) => signInWithCredentials(values))}>
+//         <TextInput
+//           withAsterisk
+//           label="Email"
+//           placeholder="your@email.com"
+//           key={form.key('email')}
+//           {...form.getInputProps('email')}
+//           mb="md"
+//         />
+//         <PasswordInput
+//           withAsterisk
+//           label="Password"
+//           placeholder="password"
+//           key={form.key('password')}
+//           {...form.getInputProps('password')}
+//         />
+//         <Box mt="md" ta='center' mb="xl" ref={ref}>
+//           {/* 未対応:ホバーした時にボタンの色を変える */}
+//           <Button type="submit" color="navy" w='100%' bg={hovered ? 'navy' : 'navy'}>
+//             ログイン
+//           </Button>
+//         </Box>
+//       </form>
+//     </Box>
+//   )
+// }
+
+// export const SignInWithCredentials = () => {
+//   const form = useForm({
+//     mode: 'uncontrolled',
+//     initialValues: {
+//       email: '',
+//       password: '',
+//     },
+//   });
+//   return (
+//     <form onSubmit={form.onSubmit(() => signInWithCredentials())}>
+//       <button type="submit">ああああ</button>
+//     </form>
+//   )
+// }
 
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,9 +1,23 @@
+'use client'
 import React from 'react'
+import { SignInWithGoogle } from '../components/sign-in'
+import { useSearchParams } from 'next/navigation'
+import { Container, Divider, Paper, Title } from '@mantine/core'
 
-const page = () => {
+const SignInPage = () => {
+  const searchParams = useSearchParams();
+  const oauthErrorMessage = searchParams.get('message');
+
   return (
-    <div>page</div>
+    <>
+      <Container size="xs" mt="5rem">
+        <Title order={2} mb="md" ta="center">ログイン</Title>
+          <Paper>
+            <Divider my="sm" />
+            <SignInWithGoogle errorMessage={oauthErrorMessage}/>  
+        </Paper>
+      </Container>
+    </>
   )
 }
-
-export default page
+export default SignInPage
