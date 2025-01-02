b6ab563cc156f96705ea3d65eea60ac950a9fe80
nkkn-prog <nkkn.prog@gmail.com>
2025-01-02 18:35:40 +0900

    refactor: フォントの変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import '@mantine/core/styles.css';
-import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
+import { Noto_Sans_JP } from "next/font/google";
+import { MantineProvider} from '@mantine/core';
+import Header from "./components/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const notoSansJP = Noto_Sans_JP({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-noto-sans-jp',
 });
 
 export const metadata: Metadata = {
@@ -28,8 +26,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning>
-          <MantineProvider>{children}</MantineProvider>
+      <body className={`${notoSansJP.variable}`} suppressHydrationWarning>
+          <MantineProvider>
+            <Header />
+            {children}
+          </MantineProvider>
       </body>
     </html>
   );
