651d7f3f27253daea79df47e8352cd77d3b373c7
nkkn-prog <nkkn.prog@gmail.com>
2025-01-10 12:33:01 +0900

    feat: 値保持の処理を追加、Create処理の追加

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -2,44 +2,66 @@
 import { Box, Button, Container, Flex, Group, MultiSelect, Paper, Text, TextInput, rem } from '@mantine/core'
 import { Dropzone } from '@mantine/dropzone'
 import { IconUpload, IconPhoto, IconX } from '@tabler/icons-react';
-import React, { useState } from 'react'
-import { Mode } from '../types'
-import Image from 'next/image';
+import React, { useState, useEffect } from 'react'
+import { Mode, ProfileValues } from '../types'
+// import Image from 'next/image';
 import RichTextEditorComponent from './tools/RichTextEditor';
-import { useForm } from 'react-hook-form';
+import { createProfile } from '../utils/profile/api';
 
 const Profile = (props: { mode: Mode }) => {
-
   // 共通部分
   const { mode } = props;
+  const [userId, setUserId] = useState<string | null>(null);
+  const [profileName, setProfileName] = useState<string>('');
+  const [profileEmail, setProfileEmail] = useState<string>('');
+  const [profileInstruments, setProfileInstruments] = useState<string[]>([]);
+  const [profileBio, setProfileBio] = useState<string>('');
+
+  useEffect(() => {
+    // クライアントサイドでユーザーIDを取得
+    const fetchUserId = async () => {
+      const response = await fetch('/api/auth/session');
+      const session = await response.json();
+      setUserId(session?.user?.id);
+    };
+    fetchUserId();
+  }, []);
 
   // TODO: 画像をアップロードした時にアップロードされた画像URLを取得し、格納する
-  const [imagePath, setImagePath] = useState<File | null>(null);
+  // const [imagePath, setImagePath] = useState<File | null>(null);
   const IMAGE_MIME_TYPE = ['image/png', 'image/jpeg', 'image/jpg', 'image/gif'];
 
   // タイトル
   const title = mode === 'create' ? 'プロフィール作成' : 'プロフィール編集';
   const buttonTitle = mode === 'create' ? '作成' : '更新';
 
-  //フォーム
-  const form = useForm();
-
   // TODO: フォームの内容を送信する
-  const handleSubmit = () => {
-    console.log('submit')
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!userId) return;
+
+    const formData: ProfileValues = {
+      userId: userId,
+      name: profileName,
+      bio: profileBio,
+      imageUrl: 'test.png',
+      instruments: profileInstruments,
+    }
+    createProfile(formData)
   }
 
   // 編集画面の処理
 
 
+  // フォーム内必須処理の追加
   return (
     <Container size='sm'>
       <Text fz="1.5rem" fw="bold" ta='center' my='2rem'>{title}</Text>
         <Paper withBorder radius='md' p='md'>
-          <form onSubmit={form.handleSubmit(handleSubmit)}>
+          <form onSubmit={(e) => handleSubmit(e)}>
             <Box mb='2rem'>
               <Text>プロフィール画像</Text>
-              <Flex direction='column' gap='1rem' justify='center' align='center'>
+              {/* <Flex direction='column' gap='1rem' justify='center' align='center'>
                 <Image
                 // TODO: アップロードされた画像のパスを格納する
                   src={imagePath ? `${imagePath}` : '/logo/google-logo.png'}
@@ -48,7 +70,7 @@ const Profile = (props: { mode: Mode }) => {
                   height={100}
                   style={{ borderRadius: '50%', border: '1px solid #ccc', objectFit: 'cover', marginBottom: '2rem'}}
                 />
-              </Flex>
+              </Flex> */}
               <Dropzone
                 maxSize={1024 * 5 * 2} 
                 accept={IMAGE_MIME_TYPE} 
@@ -87,14 +109,20 @@ const Profile = (props: { mode: Mode }) => {
               </Dropzone>
             </Box>
             <Flex direction='column' gap='1rem'>
-              <TextInput label='名前' placeholder='名前' />
-              <TextInput label='メールアドレス' placeholder='メールアドレス' />
+              <TextInput label='名前' placeholder='名前'  value={profileName} onChange={(e) => setProfileName(e.target.value)}/>
+              <TextInput label='メールアドレス' placeholder='メールアドレス' value={profileEmail} onChange={(e) => setProfileEmail(e.target.value)}/>
               <MultiSelect
                 label="出来る/やりたい楽器"
                 placeholder="楽器を選択"
                 data={['トランペット','トロンボーン','ホルン']}
+                value={profileInstruments}
+                onChange={(value) => setProfileInstruments(value)}
               />
-              <RichTextEditorComponent label='自己紹介'/>
+              <RichTextEditorComponent 
+                label='自己紹介' 
+                value={profileBio} 
+                onChange={(value) => setProfileBio(value)}
+              />
               <Button type='submit' mt='1rem' bg='navy' color='white'>{buttonTitle}</Button>
             </Flex>
           </form>
