c15c3e76b8f0ae79f41482c9b3dbc87dc6c972f6
nkkn-prog <nkkn.prog@gmail.com>
2025-01-13 16:50:46 +0900

    feat: プランを選択できるようにした

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,12 +1,10 @@
-import { Box,Flex, Text } from '@mantine/core'
+import { Box,Flex, Menu, MenuDropdown, MenuItem, MenuTarget, Text } from '@mantine/core'
 import Link from 'next/link'
 import React from 'react'
 import { auth } from '../auth'
 
 export const Header = async () => {
   const session = await auth();
-  console.log('Headerのセッション')
-  console.log(session)
 
   return (
     <Flex w="100%" h="4rem" bg="navy" display='flex' justify='space-between' align='center'>
@@ -16,9 +14,26 @@ export const Header = async () => {
         </Text>
       </Box>
       {session?.user? (
-        <Text c="white" fz="1rem" fw="bold" mr='1rem'>
-          <Link href='/signout'>ログアウト</Link>
-         </Text>
+        <Flex>
+          <Box>
+            <Menu trigger="hover" openDelay={50} closeDelay={100} position='bottom'>
+              <MenuTarget>
+                <Text c="white" fz="1rem" fw="bold" mr='1rem'>プラン設定</Text>
+              </MenuTarget>
+              <MenuDropdown>
+                {/*  */}
+                <MenuItem><Link href='user/plan/create'>プラン作成</Link></MenuItem>
+                <MenuItem><Link href='user/plan'>マイプラン一覧</Link></MenuItem>
+              </MenuDropdown>
+            </Menu>
+          </Box>
+          <Text c="white" fz="1rem" fw="bold" mr='1rem'>
+            <Link href={`/user/profile/edit/${session.user.id}`}>プロフィール設定</Link>
+          </Text>
+          <Text c="white" fz="1rem" fw="bold" mr='1rem'>
+            <Link href='/signout'>ログアウト</Link>
+          </Text>
+        </Flex>
       ) : (
         <Flex gap='1rem' align='center' mr='1rem'>
           <Text c="white" fz="1rem" fw="bold">
