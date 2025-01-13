2b924b31277c7de9e387cede9db78933028c654b
nkkn-prog <nkkn.prog@gmail.com>
2025-01-13 16:50:16 +0900

    feat: planに関する型定義を追加

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,4 +1,4 @@
-export type Mode = 'create' | 'edit' | 'view';
+export type Mode = 'create' | 'edit' | 'view' | 'myplan' |'general'
 
 export type ProfileValues = {
   userId: string;
@@ -6,4 +6,44 @@ export type ProfileValues = {
   bio: string;
   imageUrl: string;
   instruments: string[];
+}
+
+export type PlanValuesForCreate = {
+  userId: string;
+  title: string;
+  instruments: string[];
+  description: string;
+  thumbnailPath?: string;
+  contract: string;
+  price: number;
+  time: number;
+  consultation: string;
+  cancellation: boolean;
+}
+
+export type PlanValuesForUpdate = {
+  id: number | undefined;
+  title: string;
+  instruments: string[];
+  description: string;
+  thumbnailPath?: string;
+  contract: string;
+  price: number;
+  time: number;
+  consultation: string;
+  cancellation: boolean;
+}
+
+export type Plan = {
+  id: string;
+  userId: string;
+  title: string;
+  instruments: string[];
+  description: string;
+  thumbnailPath?: string;
+  contract: string;
+  price: number;
+  time: number;
+  consultation: string;
+  cancellation: boolean;
 }
