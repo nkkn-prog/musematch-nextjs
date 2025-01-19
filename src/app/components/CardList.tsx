293a4727f841c8e2865ba426b7691598276acebf
nkkn-prog <nkkn.prog@gmail.com>
2025-01-19 15:56:27 +0900

    refactor: プランの出し分けを修正

--- src/app/components/CardList.tsx
+++ src/app/components/CardList.tsx
@@ -2,23 +2,17 @@
 import { Badge, Button, Card, CardSection, Flex, Text, Box } from '@mantine/core'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
-import { getPlan } from '../utils/plan/api'
+import { getAllPlans } from '../utils/plan/api'
 import { Mode, Plan } from '../types'
 import { useRouter } from 'next/navigation'
-
-// HTMLタグを除去するヘルパー関数を追加
-const stripHtml = (html: string) => {
-  const tmp = document.createElement('div');
-  tmp.innerHTML = html;
-  return tmp.textContent || tmp.innerText || '';
-};
-
+import { stripHtml } from '../utils/page'
 const CardList = (props: { mode: Mode }) => {
   
   const { mode } = props;
   const [userId, setUserId] = useState<string>('');
   const [plans, setPlans] = useState<Plan[]>([]);
   const [myPlans, setMyPlans] = useState<Plan[]>([]);
+  const [othersPlans, setOthersPlans] = useState<Plan[]>([]);
   const router = useRouter();
 
   useEffect(() => {
@@ -34,7 +28,7 @@ const CardList = (props: { mode: Mode }) => {
   useEffect(() => {
     const fetchPlans = async () => {
       // プランを取得する
-      const plans = await getPlan();
+      const plans = await getAllPlans();
       setPlans(plans.data);
 
       // 取得したプランのうち、ユーザーIDが一致するプランを取得する
@@ -43,15 +37,22 @@ const CardList = (props: { mode: Mode }) => {
         const plansDuplicate = [...plans.data];
         const myPlans = plansDuplicate.filter((plan: Plan) => plan.userId === userId);
         setMyPlans(myPlans);
+      } else if (mode === 'general') {
+        const plansDuplicate = [...plans.data];
+        const othersPlans = plansDuplicate.filter((plan: Plan) => plan.userId !== userId && plan.cancellation === false);
+        setOthersPlans(othersPlans);
       }
+
     }
     fetchPlans()
   }, [mode, userId]);
 
-  const handleMoveToPlan = (id: number) => {
-    if(mode === 'myplan'){
+  const handleMoveToPlan = (id: number, userUuid: string) => {
+    // ユーザーIDが一致する場合は編集ページへ移動する
+    if(userId === userUuid || mode === 'myplan'){
       router.push(`/user/plan/edit/${id}`);
     }else{
+    // ユーザーIDが一致しない場合はプランページへ移動する
     router.push(`/plan/${id}`); 
     }
   }
@@ -59,7 +60,7 @@ const CardList = (props: { mode: Mode }) => {
   return (
     <Flex gap='md' wrap='wrap'>
       {mode !== 'myplan' ? (
-        plans.map((plan) => (
+        othersPlans.map((plan) => (
           <Card shadow="sm" padding="lg" radius="md" withBorder w='15rem' key={plan.id} h='22rem'>
             <CardSection>
               {/* Cardのサイズに合わせて画像のサイズを調整する 240px = 15rem * 16px */}
@@ -73,22 +74,33 @@ const CardList = (props: { mode: Mode }) => {
             </CardSection>
 
             <Box mt="xs" mb="xs" h='5rem'>
-              <Text fw={500} lineClamp={2}>{plan.title}</Text>
-              {plan.cancellation ?  <Badge color="red">申込不可</Badge> : <Badge color="pink">申込可</Badge>}
+              <Text fw={500} h='3rem' lineClamp={2}>{plan.title}</Text>
             </Box>
 
             <Text size="sm" c="dimmed" h='3rem' lineClamp={2}>
               {stripHtml(plan.description)}
             </Text>
+            {plan.userId !== userId ?(
             <Button
               color="navy"
               fullWidth
               mt="md"
               radius="md"
               h='3rem'
-              onClick={() => {handleMoveToPlan(Number(plan.id))}}
+              onClick={() => {handleMoveToPlan(Number(plan.id), plan.userId)}}
             >
               プランを見る</Button>
+            ) : (
+              <Button
+                color="navy"
+                fullWidth
+                mt="md"
+                radius="md"
+                h='3rem'
+                onClick={() => {handleMoveToPlan(Number(plan.id), plan.userId)}}
+              >
+                プランを編集する</Button>
+            )}
           </Card>
         ))
       ) : (
@@ -119,7 +131,7 @@ const CardList = (props: { mode: Mode }) => {
               mt="md"
               radius="md"
               h='3rem'
-              onClick={() => {handleMoveToPlan(Number(plan.id))}}
+              onClick={() => {handleMoveToPlan(Number(plan.id), plan.userId)}}
             >
               プランを編集する</Button>
           </Card>
--- src/app/plan/[id]/page.tsx
+++ src/app/plan/[id]/page.tsx
@@ -1,25 +1,80 @@
-import { Button, Container, Paper, Text, Title } from '@mantine/core'
+'use client'
+import { Plan } from '@/app/types';
+import { applyPlan, getPlan } from '@/app/utils/plan/api';
+import { Box, Button, Container, Paper, Text} from '@mantine/core'
 import Image from 'next/image'
-import React from 'react'
+import { useParams, useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
+import { stripHtml } from '@/app/utils/page'
 
 const PlanDetail = () => {
+  const { id } = useParams();
+  const planId = Number(id);
+  const [plan, setPlan] = useState<Plan | null>(null);
+  const [userId, setUserId] = useState<string>('');
+  const [instructorId, setInstructorId] = useState<string>('');
+  const router = useRouter();
+  const [isContract, setIsContract] = useState<boolean>(false);
+
+  useEffect(() => {
+    // クライアントサイドでユーザーIDを取得
+    const fetchUserId = async () => {
+      const response = await fetch('/api/auth/session');
+      const session = await response.json();
+      setUserId(session?.user?.id);
+    };
+    fetchUserId();
+  }, []);
+
+  // プランを取得する
+  useEffect(() => {
+    const fetchPlan = async () => {
+      const data = await getPlan(planId);
+      if(data.plan){
+        setPlan(data.plan);
+        setInstructorId(data.plan.userId);
+      }
+      if(data.contract){
+        setIsContract(true);
+      }
+    }
+    fetchPlan();
+  }, [planId]);
+
+  const handleApply = () => {
+    if(userId === instructorId){
+      router.push(`/user/plan/edit/${planId}`);
+    } else {
+
+      const applyValue = {
+        planId: planId,
+        userId: userId,
+        instructorId: instructorId,
+      }
+
+      applyPlan(applyValue);
+      router.push(`/plan`);
+    }
+  }
+
   return (
     <Container>
-      <Paper withBorder p='1rem' my='5rem' mih='70vh'>
-        <Image src="/saxophone.jpg" height={240} width={240} alt="Saxophone" style={{ objectFit: 'cover' }} />
-        {/* TODO:プラン作成で入力した内容を表示する。HTMLを変換する必要があるかも */}
-        <Text>テストテストテストテストテストテストテストテストテスト</Text>
-      </Paper>
-      {/* このユーザーのコースメニューを表示 */}
-      <Title order={3}>コースメニュー</Title>
-
-      <Paper>
-        <Title order={4}>コースのタイトル</Title>
-        <Text>コースの内容</Text>
-      </Paper>
-
-      <Paper>
-        <Button color="navy" w='25%' mt="md" radius="md">申し込む</Button>
+      <Paper withBorder p='1rem' my='5rem' >
+        <Box ta='center'>
+          <Image src="/saxophone.jpg" height={300} width={860} alt="Saxophone" style={{ objectFit: 'cover'}} />
+        </Box>
+        <Text w='100%' h='100%' mih='8rem'>{plan?.description ? stripHtml(plan.description) : ''}</Text>
+        <Box w='100%' ta='center'>
+          {isContract ?
+            <Button color="navy" w='25%' mt="md" radius="md" disabled>
+              契約済み
+            </Button>
+          :
+          <Button color="navy" w='25%' mt="md" radius="md" onClick={() => handleApply()}>
+            {userId === instructorId ? '編集する' : '申し込む'}
+          </Button>
+          }
+        </Box>
       </Paper>
 
     </Container>
