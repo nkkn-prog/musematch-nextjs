7d1e4e16d67012944ced61908ad1b0700665aa5c
nkkn-prog <nkkn.prog@gmail.com>
2025-01-24 19:32:52 +0900

    feat: signOutの画面を追加

--- src/app/signout/page.tsx
+++ src/app/signout/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+'use client'
 
-const page = () => {
+import { Box, Text, Button, Container, Paper } from '@mantine/core'
+import React from 'react'
+import { signOut } from '../auth';
+import { useRouter } from 'next/navigation';
+
+const SignOut = () => {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    await signOut();
+    router.push('/signin');
+  }
+
   return (
-    <div>page</div>
+    <Container size='100%' mih='20vh'>
+      <Paper withBorder p='1rem' my='5rem' mih='20vh'>
+        <Box ta='center'>
+          <Text fz='1.5rem' fw='bold'>サインアウトしますか？</Text>
+          <Box mt='2rem'>
+            <Button mr='3rem' onClick={() => router.push('/plan')} bg='navy'>キャンセル</Button>
+            <Button onClick={() => handleSignOut()} bg='navy'>サインアウト</Button>
+          </Box>
+        </Box>
+      </Paper>
+    </Container>
   )
 }
 
-export default page
+export default SignOut
